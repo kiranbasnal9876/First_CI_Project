d67af5785da749e1cd2c397a36624777be2c8216
kiranbasnal9876 <dimpalbasnal0@gmail.com>
2025-02-20 23:30:43 +0530

    first commit

--- application/controllers/Paggination.php
+++ application/controllers/Paggination.php
@@ -1,14 +1,27 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
+Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+Header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+Header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); //method allowed
 class Paggination extends CI_Controller
 {
         function index() {
-                $data=$_POST;
+
+            
+        }
+
+
+        function test(){
+
+                echo "hii" ;die;
+                $data = file_get_contents("php://input");
+                $data = json_decode($data, TRUE);
+                $_POST = $data;
+                // $data=$_POST;
              $table=$_POST['table_name'];
-             array_pop($data);
+             unset($data['table_name']);die;
         
         echo records($table,$data);
-            
-            
         }
 
      
